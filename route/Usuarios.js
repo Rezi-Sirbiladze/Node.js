4d50fd9b384070207e78ed8f515026b2897b0ba2
Rezi <rezi.sirbiladze2@gmail.com>
2022-05-16 13:57:46 +0200

    añadido a CRUD el delete

--- route/Usuarios.js
+++ route/Usuarios.js
@@ -1,32 +1,70 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const router = express.Router();
 const Usuario = require('../models/Usuario');
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
 
-    try{
+    try {
         const arrayUsuarios = await Usuario.find();
         res.render("usuarios", {
             arrayUsuarios
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 });
 
 router.get('/crear', (req, res) => {
     res.render('crear');
-})
+});
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const body = req.body;
-    try{
+    try {
         const usuario = new Usuario(body);
         await usuario.save();
         res.redirect('/usuarios');
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
-})
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const usuario = await Usuario.findOne({ _id: id });
+        res.render('detalle', {
+            usuario: usuario,
+            error: false
+        })
+    } catch (error) {
+        console.log(error);
+        res.render('detalle', {
+            error: true,
+            message: 'No se encuentra el usuario'
+        })
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const usuario = await Usuario.findByIdAndDelete({ _id: id });
+        if (usuario) {
+            res.json({
+                error: false,
+                message: 'Usuario eliminado'
+            });
+        } else {
+            res.json({
+                error: true,
+                message: 'Usuario no se puede eliminar'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router;
